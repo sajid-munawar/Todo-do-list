b96ccaf98514b93808b384762af8972f3ff8c683
sajid-munawar <sajidmunawar666@gmail.com>
2022-06-09 01:25:14 +0500

    Delete item on Ui

--- src/addRemove.js
+++ src/addRemove.js
@@ -3,8 +3,8 @@ const dynamicTodos = document.querySelector('.dynamic-todos');
 
 export const todos = [];
 
-const todoGenerate = ({ description }) => `
-   <div class="todo-item">
+const todoGenerate = ({ description,index }) => `
+   <div class="todo-item" id=${index}>
   <input  type="checkbox" class='checkbox'><span contenteditable="true"> ${description} </span>
   <i class="fa-solid fa-ellipsis-vertical"></i>
   <i class="fa-solid fa-trash-can"></i></div>`;
@@ -21,35 +21,51 @@ export const updateTodo = () => {
   checkboxs.forEach((c) => {
     c.addEventListener('click', () => {
       c.parentElement.classList.toggle('checked');
+      console.log(c.parentElement.id);
       c.nextElementSibling.classList.toggle('checkTodo');
       c.nextElementSibling.nextElementSibling.classList.toggle('doticon');
       c.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle('trash');
     });
   });
 
-  const spans = document.querySelectorAll('span');
-  spans.forEach((span) => {
-    span.addEventListener('click', (e) => {
-      e.target.classList.remove('focus');
-    });
-    span.addEventListener('keypress', (e) => {
-      if (e.key === 'Enter') {
-        e.target.parentElement.classList.add('edited');
-      }
-      const localData = JSON.parse(localStorage.getItem('todos'));
-
-      for (let i = 0; i < spans.length; i += 1) {
-        if (spans[i].parentElement.classList.contains('edited')) {
-          localData[i].description = e.target.textContent;
-          localStorage.setItem('todos', JSON.stringify(localData));
-
-          window.getSelection().removeAllRanges();
+  //Function to edit description
+  const editDesc = () =>{
+    const spans = document.querySelectorAll("span");
+    spans.forEach((span) => {
+      span.addEventListener("click", (e) => {
+        e.target.classList.remove("focus");
+      });
+      span.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+          e.target.parentElement.classList.add("edited");
         }
-      }
-      e.target.parentElement.classList.remove('edited');
-      e.target.classList.add('focus');
+        const localData = JSON.parse(localStorage.getItem("todos"));
+
+        for (let i = 0; i < spans.length; i += 1) {
+          if (spans[i].parentElement.classList.contains("edited")) {
+            localData[i].description = e.target.textContent;
+            localStorage.setItem("todos", JSON.stringify(localData));
+
+            window.getSelection().removeAllRanges();
+          }
+        }
+        e.target.parentElement.classList.remove("edited");
+        e.target.classList.add("focus");
+      });
     });
-  });
+  }
+  editDesc();
+  // Function to Delete todo
+  const delTodo = () => {
+    const trashs = document.querySelectorAll(".fa-trash-can");
+    trashs.forEach((trash,index) => {
+      trash.addEventListener('click', (e) => {
+        console.log('t',index);
+        dynamicTodos.removeChild(e.target.parentElement)
+      })
+    })
+  }
+  delTodo();
 };
 updateTodo();
 

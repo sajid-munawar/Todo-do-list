20de4ebf5257a523c0da044592071dc6dd31499b
sajid-munawar <sajidmunawar666@gmail.com>
2022-06-08 18:28:56 +0500

    Fix eslint errors

--- src/addRemove.js
+++ src/addRemove.js
@@ -1,7 +1,6 @@
 const form = document.querySelector('form');
 const dynamicTodos = document.querySelector('.dynamic-todos');
 
-
 export const todos = [];
 
 const todoGenerate = ({ description }) => `
@@ -9,6 +8,29 @@ const todoGenerate = ({ description }) => `
   <input  type="checkbox" class='checkbox'><span> ${description} </span>
   <i class="fa-solid fa-ellipsis-vertical"></i>
   <i class="fa-solid fa-trash-can"></i></div>`;
+
+const changeTask = (todoContainer, todo) => {
+  const editInput = document.createElement('input');
+  editInput.type = 'text';
+  editInput.className = 'editInput';
+  editInput.value = todo.textContent;
+  todoContainer.replaceChild(editInput, todo);
+  editInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      const todoContainers = document.querySelectorAll('.todo-item');
+      const localData = JSON.parse(localStorage.getItem('todos'));
+      for (let i = 0; i < todoContainers.length; i += 1) {
+        if (todoContainers[i].classList.contains('edit-clicked')) {
+          localData[i].description = editInput.value;
+          localStorage.setItem('todos', JSON.stringify(localData));
+        }
+      }
+      editInput.parentElement.classList.remove('edit-clicked');
+      todoContainer.replaceChild(todo, editInput);
+      todo.textContent = editInput.value;
+    }
+  });
+};
 
 const todosFromLocalStorage = JSON.parse(localStorage.getItem('todos'));
 
@@ -19,50 +41,26 @@ export const updateTodo = () => {
     dynamicTodos.innerHTML = todos.map((todo) => todoGenerate(todo)).join('');
     localStorage.setItem('todos', JSON.stringify(todos));
   }
-    const checkboxs = document.querySelectorAll('.checkbox')
-    checkboxs.forEach(c => {
-        c.addEventListener('click', () => {
-          c.parentElement.classList.toggle("checked");
-          c.nextElementSibling.classList.toggle("checkTodo");
-          c.nextElementSibling.nextElementSibling.classList.toggle('doticon')
-          c.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle('trash')
-        })
-    })
+  const checkboxs = document.querySelectorAll('.checkbox');
+  checkboxs.forEach((c) => {
+    c.addEventListener('click', () => {
+      c.parentElement.classList.toggle('checked');
+      c.nextElementSibling.classList.toggle('checkTodo');
+      c.nextElementSibling.nextElementSibling.classList.toggle('doticon');
+      c.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle('trash');
+    });
+  });
 
-    const editIcons = document.querySelectorAll(".fa-ellipsis-vertical");
-    editIcons.forEach(c => {
-        c.addEventListener('click', () => {
-            const todoContainer = c.parentElement
-            todoContainer.classList.add("edit-clicked");
-            changeTask(todoContainer,c.previousElementSibling);
-        })
-    })
+  const editIcons = document.querySelectorAll('.fa-ellipsis-vertical');
+  editIcons.forEach((c) => {
+    c.addEventListener('click', () => {
+      const todoContainer = c.parentElement;
+      todoContainer.classList.add('edit-clicked');
+      changeTask(todoContainer, c.previousElementSibling);
+    });
+  });
 };
 updateTodo();
-
-const changeTask = (todoContainer,todo) => {
-    const editInput = document.createElement('input')
-    editInput.type = 'text'
-    editInput.className = 'editInput'
-    editInput.value = todo.textContent
-    todoContainer.replaceChild(editInput, todo)
-    editInput.addEventListener('keypress', e => {
-        if (e.key === 'Enter') {
-            const todoContainers = document.querySelectorAll('.todo-item')
-            const localData = JSON.parse(localStorage.getItem('todos'))
-            for (let i = 0; i < todoContainers.length; i += 1){
-                if (todoContainers[i].classList.contains('edit-clicked')) {
-                    localData[i].description = editInput.value
-                    localStorage.setItem('todos',JSON.stringify(localData))
-                }
-            }
-            editInput.parentElement.classList.remove('edit-clicked')
-            todoContainer.replaceChild(todo, editInput);
-            todo.textContent=editInput.value
-
-        }
-    })
-}
 
 export const addTodo = () => {
   if (form.add.value.trim()) {
@@ -85,4 +83,3 @@ export const addTodo = () => {
     }
   }
 };
-

297a03f3cd10f366f60d51f1e3090c59fddf7e11
sajid-munawar <sajidmunawar666@gmail.com>
2022-06-08 13:56:42 +0500

    Remove linter errors for add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import './style.css';
-import {addTodo,updateTodo } from './addRemove';
-const form = document.querySelector("form");
+import { addTodo } from './addRemove.js';
 
+const form = document.querySelector('form');
 
-
-
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   addTodo();
-  form.reset()
+  form.reset();
 });
